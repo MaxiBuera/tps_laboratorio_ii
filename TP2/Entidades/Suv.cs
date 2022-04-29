6683f3ad60d4371ba9e2e5df2eb4bd0c6c2e621d
MaxiBuera <maxibuera@gmail.com>
2022-04-29 18:38:08 -0300

    tp2 primer commit

---
+++ TP2/Entidades/Suv.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Suv : Vehiculo
+    {
+        public Suv(string chasis, EMarca marca, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+        }
+        /// <summary>
+        /// SUV son 'Grande'
+        /// </summary>
+        public override ETamanio Tamanio
+        {
+            get
+            {
+                return this.Tamanio;
+            }
+        }
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SUV");
+            ////sb.AppendLine(base);
+            sb.AppendLine($"TAMAÑO : {this.Tamanio}");
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
